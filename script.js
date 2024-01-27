9296d52d75708775bf075a7bde818a6b4d46ff1b
bharatbhatt101 <bharatbhatt2003bk@gmail.com>
2024-01-27 11:24:16 +0530

    ok

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ let string = "";
 let arr = Array.from(button);
 arr.forEach(button => {
     button.addEventListener("click", (e) =>{
-        if (e.target.innerHTML == '=') {
+        if (e.target.innerHTML == '') {
             string = eval(string);
             input.value = string;
         }
         
-         else if(e.target.innerHTML=='Call')
+         else if(e.target.innerHTML=='X'|| e.target.innerHTML=='End' || e.target.innerHTML=='Call')
          {
              string="";
              input.value=string;
 
          }
 
-         else if(e.target.innerHTML=='X')
+         else if(e.target.innerHTML=='')
          {
                  string=string.substring(0,string.length-1);
                  input.value=string;
